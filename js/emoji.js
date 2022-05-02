8f17876cbac253d4ce725f02cf3cf828dc09b3b0
Marina <mazhivotova@gmail.com>
2022-05-03 00:47:28 +0300

    final version

--- js/emoji.js
+++ js/emoji.js
@@ -1,96 +1,57 @@
 import { data } from "./data.js";
 
-console.log(data);
-
-
-
-function createSome(icon, text, desc) {
-
-
-    let newDiv = document.createElement("div")
-    let newDivInside = document.createElement("div")
-    let newh2 = document.createElement("h2")
-    let newh3 = document.createElement("h3")
-
-    newDiv.className = "emoji__block"
-
-
+/////////////////////////////////// one emoji card creation
+function createOneCard(icon, text, desc) {
     let main = document.getElementsByTagName("main")
-    // console.dir(main);
 
-    main[0].append(newDiv)
-
-    newDiv.append(newDivInside);
-    newDiv.append(newh2);
-    newDiv.append(newh3);
-
-    newDivInside.innerHTML = icon
-    newh2.innerText = text
-    newh3.innerText = desc
-
-}
-// console.log(createSome("&#129409", "ddd", "yyy"));
-
-
-
-function createSome00(icon, text, desc) {
-    let main = document.getElementsByTagName("main")
-    // main.className = "emoji__flex"
-    // main.classList.add("main__flex")
     let newDiv2 = document.createElement("div")
     newDiv2.className = "emoji__block"
     main[0].append(newDiv2)
     newDiv2.insertAdjacentHTML("afterbegin", "<h3>" + desc + "</h3>")
     newDiv2.insertAdjacentHTML("afterbegin", "<h2>" + text + "</h2>")
     newDiv2.insertAdjacentHTML("afterbegin", "<div  class = 'image_class'>" + icon + "</div >")
-
-
-
-
 }
-// console.log(createSome00("&#129409", "aaaa", "Hundred, points, symbol, wow, win, perfect, parties"));
-// console.log(createSome00("&#129409", "aaaa", "Hundred, points, symbol, wow, win, perfect, parties"));
-// console.log(createSome00("&#129409", "aaaa", "Hundred, points, symbol, wow, win, perfect, parties"));
-// console.log(createSome00("&#129409", "aaaa", "Hundred, points, symbol, wow, win, perfect, parties"));
-// console.log(createSome00("&#129409", "aaaa", "Hundred, points, symbol, wow, win, perfect, parties"));
-// console.log(createSome00("&#129409", "aaaa", "Hundred, points, symbol, wow, win, perfect, parties"));
+
+///////////////////////////////take away the repetitive words in cards
+let newData = data;
+newData.forEach(element => {
+    for (let key in element) {
+        if (key == "keywords") {
+            element[key] = [...new Set(element[key].split(' '))].join(' ');
+        }
+    };
+    return element;
+
+});
 
 
-
+/////////////////////////////creation of all emoji cards
 function createCards() {
-    data.forEach(element => {
-        createSome00(element.symbol, element.title, element.keywords)
+    newData.forEach(element => {
+        createOneCard(element.symbol, element.title, element.keywords)
     });
     return;
 }
 createCards()
 
-
+/////////////////////// searching by keywords and title
 const searchInput = document.querySelector(".search");
-
-// searchInput.addEventListener('change', () => inputHandler(event));
 searchInput.addEventListener('input', (event) => inputHandler(event));
 
-let allCards = document.getElementsByClassName("emoji__block")
 function inputHandler(event) {
-    console.log(event.target.value);
 
-    // console.dir(allCards);
-    if (event.target.value.length > 2) {
-        let item
-        for (item of allCards) {
-            if (item.innerText.includes(event.target.value)) {
-                item.style.display = "block";
-            } else {
-                item.style.display = "none";
-            }
+    let sort = data.filter(
+        (elem) =>
+            elem.title.includes(event.target.value) ||
+            elem.keywords.includes(event.target.value)
+    );
+    document.querySelector("main").innerHTML = "";
+    getSortData(sort);
 
-            // console.dir(item.innerText);
-        }
-
-    }
 }
 
-
-
-
+let getSortData = (sort) => {
+    sort.forEach((element) => {
+        createOneCard(element.symbol, element.title, element.keywords);
+    });
+}
